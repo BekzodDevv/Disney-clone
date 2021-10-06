ca0981a774c52f94f74777373d645ba414bf75ff
BekzodDevv <bekzodabdulla04@gmail.com>
2021-10-06 18:09:51 +0500

    version 1.0.1

--- pages/index.js
+++ pages/index.js
@@ -36,8 +36,14 @@ export default function Home({
         after:bg-fixed after:absolute after:inset-0 after:z-[-1]">
           <Slider />
           <Brands />
-          <MoviesCollection results={popularMovies} title="pop" />
-          <ShowsCollection results={popularShows} title="pop" />
+          <MoviesCollection results={popularMovies} title="Popular Movies" />
+          <ShowsCollection results={popularShows} title="Popular Shows" />
+
+          <MoviesCollection
+            results={top_ratedMovies}
+            title="Top Rated Movies"
+          />
+          <ShowsCollection results={top_ratedShows} title="Top Rated Shows" />
 
         </main>
       )}
